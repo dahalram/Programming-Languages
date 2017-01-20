35de88ed27e3bcef66be1c7b512fc761689648e0
Ram Hari Dahal <ramhari.dahal1@gmail.com>
2017-01-20 14:19:53 -0500

    Functions skeletons

--- Functional Programming Assignment/fp.rkt
+++ Functional Programming Assignment/fp.rkt
@@ -1,1 +1,25 @@
 ;;First commit
+(DEFINE (reverse-general L)
+        (reverse-general (CDR()) )
+
+
+        
+)
+
+(DEFINE (sum-up-numbers-simple L)
+
+
+
+)
+
+(DEFINE (sum-up-numbers-general L)
+
+
+
+)
+
+(DEFINE (min-above-min L1 L2)
+
+
+
+)
