280d1f4e183f43edd751fdf5db06e7abbf554af7
Ram Hari Dahal <ramhari.dahal1@gmail.com>
2017-02-05 21:14:46 -0500

    Check if num

--- Logic Programming Assignment/lp.pl
+++ Logic Programming Assignment/lp.pl
@@ -4,7 +4,11 @@
 
 # sum-up-numbers-simple(L, N) :- 
 
+is_number(X, 0) :- 
+	not(number(X)).
+
 list_sum([], 0).
 list_sum([Head | Tail], Sum) :-
     list_sum(Tail, RestSum),
     Sum is Head + RestSum.
+
--- Logic Programming Assignment/test.pl
+++ Logic Programming Assignment/test.pl
@@ -1,4 +1,16 @@
+
+list_check(X, 0) :- 
+	is_list(X).
+
+is_number(X, 0) :- 
+	not(number(X)).
+
 sum_list([], 0).
 sum_list([H|T], Sum) :-
-   sum_list(T, Rest),
-   Sum is H + Rest.
+	sum_list(T, Rest),
+	not(is_list(H)),
+	is_number(H, 0),
+	Sum is H + Rest.
+
+% check_val([], 0).
+% check_val()
