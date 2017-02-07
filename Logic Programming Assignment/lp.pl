a9b875fd625b21b84af110dff86cc4ad3e089597
Ram Hari Dahal <ramhari.dahal1@gmail.com>
2017-02-06 22:39:41 -0500

    Comments and pseudo-code added

--- Logic Programming Assignment/lp.pl
+++ Logic Programming Assignment/lp.pl
@@ -27,14 +27,24 @@ sum-up-numbers-simple([Head | Tail], Sum) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 2 sum-up-numbers-general(L, N).
 
+% Pseudo-code
+% Check if the list is empty
+% Check if the element is a number
+% Check if the element is a list
+% Check if the element is neither a number nor a list
+% Add the sum and recurse according to what the element is.
+
+% Return 0 for empty list
 sum-up-numbers-general([], 0).
 
+% If the element is not a number and not a list, then return 0
 sum-up-numbers-general([Head | Tail], Sum) :- 
 	not(number(Head)),
 	not(is_list(Head)),
 	sum-up-numbers-general(Tail, RestSum),
 	Sum is 0 + RestSum.
 
+% If the element is not a number but a list, do a recursion to get the sum from that list element
 sum-up-numbers-general([Head | Tail], Sum) :- 
 	not(number(Head)),
 	is_list(Head),
@@ -42,6 +52,7 @@ sum-up-numbers-general([Head | Tail], Sum) :-
 	sum-up-numbers-general(Tail, TailSum),
 	Sum is HeadSum + TailSum.
 
+% If the element is just a number, add it to the sum
 sum-up-numbers-general([Head | Tail], Sum) :- 
 	number(Head),
 	not(is_list(Head)),
